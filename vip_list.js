69c9711456fc7a9daefcbcecdbc13e521fc8dd7a
jun5238 <cndrone@naver.com>
2026-03-20 08:42:19 +0900

    Update vip_list.js

--- vip_list.js
+++ vip_list.js
@@ -12,8 +12,8 @@ const VIP_COMPANY_DATA = [
     { id: "lhc3102", name: "이한철", company: "HO" },
     { id: "bear42b2", name: "황인호", company: "일성" },
     { id: "manoflogistics", name: "이영수", company: "모벤티스" },
-    { id: "skycbtt", name: "최영환", company: "라이언엘" }
-    
+    { id: "skycbtt", name: "최영환", company: "라이언엘" },
+    { id: "sthlks1", name: "이기숙", company: "모벤티스" }
     
     // 새로운 기사님은 이 아래에 계속 추가해주세요!
 ];
