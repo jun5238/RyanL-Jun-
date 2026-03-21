0d4fda20c9bd30ea5eb3ec67b76aea26a75b955a
jun5238 <cndrone@naver.com>
2026-03-22 06:56:40 +0900

    Update vip_list.js

--- vip_list.js
+++ vip_list.js
@@ -1,6 +1,3 @@
-// ==========================================
-// 1. 통합 승인 명단 (여기에만 적으시면 됩니다!)
-// ==========================================
 const VIP_COMPANY_DATA = [
     { id: "julee82", name: "이준희", company: "라이언엘" },
     { id: "hojingi0723", name: "호진기", company: "라이언엘" },
@@ -16,16 +13,10 @@ const VIP_COMPANY_DATA = [
     { id: "sthlks1", name: "이기숙", company: "모벤티스" },
     { id: "rudghk3034", name: "박경화", company: "모벤티스" },
     { id: "jsa0524", name: "주혜숙", company: "유니스피어" },
-    { id: "Rokmca12", name: "신설아", company: "모벤티스" }
-
-    
-    // 새로운 기사님은 이 아래에 계속 추가해주세요!
+    { id: "Rokmca12", name: "신설아", company: "모벤티스" },
+    { id: "sjhm0416", name: "장현미", company: "모벤티스" }
 ];
 
-// ==========================================
-// 2. 🚨 아래 코드는 절대 지우거나 수정하지 마세요! 🚨
-// 기사님이 위에 적은 데이터에서 '아이디'만 쏙 뽑아서 기존 시스템으로 넘겨주는 마법 코드입니다.
-// ==========================================
 const VIP_LIST = VIP_COMPANY_DATA.map(function(user) { 
     return user.id; 
 });
